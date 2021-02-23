004809db6cbbccb70f50be5a0321d483ec669b5a
Abhay Raghuvanshi <abhayaman669@gmail.com>
2021-02-23 18:01:35 +0530

    updated code to take input and gen no randomly

--- basic_neural_network.py
+++ basic_neural_network.py
@@ -1,6 +1,17 @@
-inp = [1, 2, 3]        # list of inputs
-weight = [10, 20, 30]  # weights for each input
-bias = 2               # bias (1 bias for 1 neuron)
+import random
+
+
+# Taking n from user
+n = int(input("Enter the let of inp/weight you want: "))
+
+inp    = [random.randint(0, 10) for i in range(n)] # list of inputs
+weight = [random.randint(0, 10) for i in range(n)] # weights for each input
+bias   = random.randint(0, 5)                      # bias (1 bias for 1 neuron)
+
+# Printing all the info
+print("\ninputs are: ", inp)
+print("weights are: ", weight)
+print("bias is: ", bias)
 
 """
 calculating output i.e.
@@ -9,4 +20,4 @@ sum of all inp[i] * weight[i] + bias
 here i is from 0 to the length of inp. here it's 3
 """
 output = sum([i*j for i, j in zip(inp, weight)]) + bias
-print(output)
+print("="*20, "\nCalculated output is: ", output)
