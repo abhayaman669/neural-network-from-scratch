110a537220cac1767486a7cc525b6a10a1cd5d18
Abhay Raghuvanshi <abhayaman669@gmail.com>
2021-02-23 19:03:19 +0530

    init

---
+++ numpy_neuron/single_neuron_network.py
@@ -0,0 +1,25 @@
+import random
+
+import numpy as np
+
+# Taking n from user
+n = int(input("Enter the let of inp/weight you want: "))
+
+inp    = [random.randint(0, 10) for i in range(n)] # list of inputs
+weight = [random.randint(0, 10) for i in range(n)] # weights for each input
+bias   = random.randint(0, 5)                      # bias (1 bias for 1 neuron)
+
+# Printing all the info
+print("\ninputs are: ", inp)
+print("weights are: ", weight)
+print("bias is: ", bias)
+
+"""
+calculating output i.e.
+
+sum of all inp[i] * weight[i] + bias
+here i is from 0 to the length of inp. here it's 3
+"""
+
+output = np.dot(inp, weight) + bias
+print("="*20, "\nCalculated output is: ", output)
