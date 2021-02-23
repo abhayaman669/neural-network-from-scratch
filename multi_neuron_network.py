76d07aa3d6d8fc3d22314b6c3b16221b417de981
Abhay Raghuvanshi <abhayaman669@gmail.com>
2021-02-23 18:13:46 +0530

    Minor Fix

--- multi_neuron_network.py
+++ multi_neuron_network.py
@@ -7,7 +7,7 @@ nu = int(input("Enter the no. of neuron you want: "))
 
 inp    = [random.randint(0, 10) for i in range(n)] # list of inputs
 weight = [[random.randint(0, 10) for i in range(n)] for j in range(nu)] # weights for each neuron
-bias   = random.randint(0, 5) # bias (1 bias for 1 neuron)
+bias   = [random.randint(0, 5) for i in range(nu)] # bias (1 bias for 1 neuron)
 
 # Printing all the info
 print("\ninputs are: ", inp)
@@ -22,5 +22,5 @@ for each neuron we will have different weight
 so our output will be like: [neuron_1_output, neuron_2_output, ...]
 here i is from 0 to the length of inp. here it's 3
 """
-output = [(sum([i*j for i, j in zip(inp, w)]) + bias) for w in weight]
+output = [(sum([i*j for i, j in zip(inp, w)]) + b) for w, b in zip(weight, bias)]
 print("="*20, "\nCalculated output is: ", output)
